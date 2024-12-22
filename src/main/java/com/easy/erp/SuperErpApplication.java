ce13b40cb2a50cc0d67ce00f82468c8d6768a4bf
ahmed-bulbul <bulbulahmed.cse@yahoo.com>
2024-12-22 21:38:04 +0600

    Setup Theme

---
+++ src/main/java/com/easy/erp/SuperErpApplication.java
@@ -0,0 +1,13 @@
+package com.easy.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easy/erp/SuperErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
